37530451b9af3845d5bf3e11d177b824fa7b0030
Tim Wenzel <48419301+TimWenz3l@users.noreply.github.com>
2023-08-18 13:14:57 +0200

    Update CreateFoldersFromCSV.ps1
    Made universal added info.txt

--- CreateFoldersFromCSV.ps1
+++ CreateFoldersFromCSV.ps1
@@ -1,14 +1,15 @@
-$csvfilepath = "C:\SharedDirectory\Gesamtliste.csv"
-$folderpath = "C:\SharedDirectory\Mandanten"   
+$csvfilepath = "C:\SharedDirectory\List.csv" #Give locaction of CSV-File
+$folderpath = "C:\SharedDirectory\Accounts"   #Give Location where Folders will be created
 
-$foldernames = Import-csv -Delimiter ";" $csvfilepath -Header "MGNr"  | Select-Object -ExpandProperty MGNr
+$foldernames = Import-csv -Delimiter ";" $csvfilepath -Header "Foldername"  | Select-Object -ExpandProperty FolderName #Specify Delimiter 
 
-foreach ($MGNr in $foldernames){
-    $Folderfullpath = Join-Path $folderpath $MGNr
+foreach ($foldername in $foldernames){
+    $Folderfullpath = Join-Path $folderpath $foldername
     if(!(Test-Path -Path $Folderfullpath -PathType Container)){
-        New-item -ItemType Directory -Path $Folderfullpath
+        New-Item -ItemType Directory -Path $Folderfullpath #Creates Folders 
+        New-Item -Path $Folderfullpath -Name $foldername"_Info.txt" -ItemType "file" #Creates Info .txt Files for each Account
         Write-Host "Folder created successfully!"
     }else{
-        Write-Host "Folder &MGNr already exits!"
+        Write-Host "Folder already exits!"
     }
 }
